fdea1df0dbf1cc9bf8610f181a00b6de54e90f75
Christian Folini <christian.foloini@netnea.com>
2024-11-15 10:29:03 +0100

    feat: make max delay configurable

--- plugins/chaos-fortress-pause.lua
+++ plugins/chaos-fortress-pause.lua
@@ -2,11 +2,11 @@ function main()
     
     math.randomseed(os.time())
 
-    max_delay_seconds = 10
+    local max_delay_seconds = m.getvar("tx.cf_max_delay")
 
     delay_seconds = tostring(math.random(1, 10 * max_delay_seconds) / 10)
 
-    m.log(2, string.format("Chaos Fortress plugin delaying %s seconds", delay_seconds))
+    m.log(2, string.format("Chaos Fortress plugin delaying %s seconds (max %s)", delay_seconds, max_delay_seconds))
 
     os.execute("sleep " .. delay_seconds)
 
