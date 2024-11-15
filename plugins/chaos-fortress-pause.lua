508910b17055005efb49a910405b5155cdb91b2f
Christian Folini <christian.foloini@netnea.com>
2024-11-15 10:11:38 +0100

    fix: rename files

--- plugins/chaos-fortress-pause.lua
+++ plugins/chaos-fortress-pause.lua
@@ -6,7 +6,7 @@ function main()
 
     delay_seconds = tostring(math.random(1, 10 * max_delay_seconds) / 10)
 
-    m.log(2, string.format("Fortress of Chaos plugin delaying %s seconds", delay_seconds))
+    m.log(2, string.format("Chaos Fortress plugin delaying %s seconds", delay_seconds))
 
     os.execute("sleep " .. delay_seconds)
 
