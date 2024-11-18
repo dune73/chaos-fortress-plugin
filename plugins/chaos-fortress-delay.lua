e6f4f5be7c009cb31f5c7fedc6a29b2575a7cb8c
dune73 <email_github.com@tikon.ch>
2024-11-18 23:42:32 +0100

    feat: More features

--- plugins/chaos-fortress-delay.lua
+++ plugins/chaos-fortress-delay.lua
@@ -3,10 +3,12 @@ function main()
     math.randomseed(os.time())
 
     local max_delay_seconds = m.getvar("tx.cf_max_delay")
+    local random = m.getvar("tx.cf_delay_random_num")
 
-    delay_seconds = tostring(math.random(1, 10 * max_delay_seconds) / 10)
+    -- delay_seconds = tostring(math.random(1, 10 * max_delay_seconds) / 10)
+    delay_seconds = max_delay_seconds / 10.0 * random
 
-    m.log(2, string.format("Chaos Fortress plugin delaying %s seconds (max %s)", delay_seconds, max_delay_seconds))
+    m.log(2, string.format("Chaos Fortress plugin delaying %s seconds (max %s, random %s)", delay_seconds, max_delay_seconds, random))
 
     os.execute("sleep " .. delay_seconds)
 
